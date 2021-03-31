889c699ce96a74c8b8e2b37845de7720fbfd4fb9
cycawy <cycawy@gmail.com>
2021-03-30 22:17:09 -0700

    updated MessageForm component

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,15 +1,28 @@
 import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
+const MessageForm = (props) => {
+    const [ value, setValue ] = useState('');
+    const { chatID, creds } = props;
 
-const MessageForm = () => {
-    const [ value, setValue ] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-    const handleSubmit = () => {
+        const text = value.trim();
+        if(text.length > 0) sendMessage(creds, chatID, { text });
 
+        setValue('');
     }
 
-    const handleChange = () => {
+    const handleChange = (e) => {
+        setValue(e.target.value);
 
+        isTyping(props, chatID)
+    }
+
+    const handleUpload = (e) => {
+        sendMessage(creds, chatID, { files: e.target.files, text: '' });
     }
 
     return(
@@ -21,6 +34,21 @@ const MessageForm = () => {
                 onChange = {handleChange}
                 onSubmit = {handleSubmit}
             ></input>
+            <label htmlFor='upload-button'>
+                <span className='image-button'>
+                    <PictureOutlined className='picture-icon'/>
+                </span>
+            </label>
+            <input
+                type = 'file'
+                multiple = {false}
+                id = 'upload-button'
+                style ={{display: 'none'}}
+                onChange = { handleUpload }
+            ></input>
+            <button type='submit' className='send-button'>
+                <SendOutlined className='send-icon'/>
+            </button>
         </form>
     )
 }
